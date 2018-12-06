4da709dc2a14c32f6f92bfa706ea471ab673c6d6
Jorge Sánchez <xurxosf@gmail.com>
2018-12-06 09:28:56 +0100

    Add common classes

--- library/src/main/kotlin/org/eyeseetea/dhis2/lightsdk/common/models/D2ErrorBody.kt
+++
@@ -1,8 +0,0 @@
-package org.eyeseetea.dhis2.lightsdk.common.models
-
-data class D2ErrorBody(
-    val httpStatus: String,
-    val httpStatusCode: Int,
-    val status: String,
-    val message: String
-)
