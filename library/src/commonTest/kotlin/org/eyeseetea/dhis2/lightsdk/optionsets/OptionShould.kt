6cd2f91abe414ecd575c8abef61f6cf8650ac000
idelcano <ignaciodelcano@gmail.com>
2018-12-20 16:50:07 +0100

    I removed the variables with the option.json and read it from file

--- library/src/commonTest/kotlin/org/eyeseetea/dhis2/lightsdk/optionsets/OptionShould.kt
+++ library/src/commonTest/kotlin/org/eyeseetea/dhis2/lightsdk/optionsets/OptionShould.kt
@@ -1,6 +1,7 @@
 package org.eyeseetea.dhis2.lightsdk.optionsets
 
 import kotlinx.serialization.json.JSON
+import org.eyeseetea.dhis2.lightsdk.common.readFile
 import kotlin.test.Test
 import kotlin.test.assertNotNull
 
@@ -8,32 +9,16 @@ class OptionShould {
     @Test
     @kotlin.js.JsName("parse_with_null_code")
     fun `parse json string with null code`() {
-        val optionJson = "{\n" +
-                    "\"lastUpdated\": \"2017-09-26T10:02:40.880\",\n" +
-                    "\"code\": \"\",\n" +
-                    "\"created\": \"2017-09-26T10:02:38.820\",\n" +
-                    "\"name\": \"SW-Female & Male Sex worker\",\n" +
-                    "\"id\": \"bT503k26qdg\",\n" +
-                    "\"displayName\": \"SW-Female & Male Sex worker\",\n" +
-                "}"
-
-        val option = JSON.nonstrict.parse(Option.serializer(), optionJson)
-
+        val file = readFile("option_without_code.json")
+        val option = JSON.nonstrict.parse(Option.serializer(), file)
         assertNotNull(option)
     }
 
     @Test
     @kotlin.js.JsName("parse_without_code_field")
     fun `parse json string without code field`() {
-        val optionJson = "{\n" +
-                "\"lastUpdated\": \"2017-09-26T10:02:40.880\",\n" +
-                "\"created\": \"2017-09-26T10:02:38.820\",\n" +
-                "\"name\": \"SW-Female & Male Sex worker\",\n" +
-                "\"id\": \"bT503k26qdg\",\n" +
-                "\"displayName\": \"SW-Female & Male Sex worker\",\n" +
-                "}"
-
-        val option = JSON.nonstrict.parse(Option.serializer(), optionJson)
+        val file = readFile("option.json")
+        val option = JSON.nonstrict.parse(Option.serializer(), file)
 
         assertNotNull(option)
     }
