20472b696634627dc4aa62f2ae99ddc186e889cc
Jorge Sánchez <xurxosf@gmail.com>
2018-12-21 07:25:14 +0100

    Add endpoints to request dropdown in android and web samples

--- androidSample/src/main/java/eyeseetea/org/dhis2_light_sdk/MainActivity.kt
+++ androidSample/src/main/java/eyeseetea/org/dhis2_light_sdk/MainActivity.kt
@@ -6,27 +6,51 @@ import eyeseetea.org.dhis2_light_sdk.hideKeyboard
 import kotlinx.android.synthetic.main.activity_main.*
 import org.eyeseetea.dhis2.lightsdk.D2Api
 import org.eyeseetea.dhis2.lightsdk.D2Response
+import android.widget.ArrayAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    private var d2Api: D2Api = D2Api.Builder().build()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val arrayAdapter =
+            ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item,
+                getEndpoints().keys.toList()
+            )
+
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        endpointSpinner.adapter = arrayAdapter
+
         requestButton.setOnClickListener {
             passwordEditText.hideKeyboard()
 
-            val d2Api = D2Api.Builder()
+            d2Api = D2Api.Builder()
                 .url(urlEditText.text.toString())
                 .credentials(usernameEditText.text.toString(), passwordEditText.text.toString())
                 .apiVersion(apiVersionEditText.text.toString())
-                .build();
-            val response = d2Api.optionSets().getAll().execute()
+                .build()
 
-            when (response) {
-                is D2Response.Success -> helloTextView.text = response.value.toString()
-                is D2Response.Error -> helloTextView.text = response.toString()
-            }
+            executeRequest()
         }
     }
+
+    private fun executeRequest() {
+        val endpointsMap = getEndpoints()
+        val name = endpointSpinner.selectedItem.toString()
+        val response: D2Response<Any>? = endpointsMap[name]!!.execute()
+
+        when (response) {
+            is D2Response.Success -> helloTextView.text = response.value.toString()
+            is D2Response.Error -> helloTextView.text = response.toString()
+        }
+    }
+
+    private fun getEndpoints() =
+        mapOf(
+            "SystemInfo" to d2Api.appInfo().get(),
+            "OptionSets" to d2Api.optionSets().getAll())
 }
--- webSample/js/sample.js
+++ webSample/js/sample.js
@@ -1,29 +1,69 @@
 "use strict";
 
+var library = require('library');
+
+var lightsdk = library.org.eyeseetea.dhis2.lightsdk
+
+var d2Api =  (new lightsdk.D2Api.Builder()).build();
+
+var endpoints = loadEndpoints()
+
 window.onload=function() {
-    document.getElementById('login-form').onsubmit=function() {
+
+    populateEndpointsSelect();
+
+    document.getElementById('login-form').onsubmit = function() {
 
         document.getElementById("result").innerText = "";
-
-        var library = require('library');
-
-        var lightsdk = library.org.eyeseetea.dhis2.lightsdk
 
         var credentials = new lightsdk.D2Credentials(
                 document.getElementById("usernameInput").value,
                 document.getElementById("passwordInput").value)
 
-        var d2Api = new lightsdk.D2Api(
+        d2Api = new lightsdk.D2Api(
                 document.getElementById("urlInput").value,
                 credentials,
                 document.getElementById("apiVersion").value);
 
-        d2Api.optionSets().getAll().execute()
+        var endpointSelect = document.getElementById("endpoint-select");
+        var key =  endpointSelect.options[endpointSelect.selectedIndex].value;
+
+        var endpoint = endpoints[key]
+
+        endpoint().execute()
             .then((response) => {
                 document.getElementById("result").innerText =response.toString();
             });
 
         // You must return false to prevent the default form behavior
         return false;
-  }
+    }
+}
+
+function populateEndpointsSelect(){
+    var endpointSelect = document.getElementById("endpoint-select");
+
+    Object.keys(endpoints).forEach(function(key,index) {
+       var opt = document.createElement("option");
+
+       opt.innerHTML = key;
+       opt.value = key
+
+       // then append it to the select element
+       endpointSelect.appendChild(opt);
+    });
+}
+
+function loadEndpoints(){
+    var endpoints = {};
+
+    endpoints["SystemInfo"] = function () {
+        return d2Api.appInfo().get();
+        };
+
+    endpoints["OptionSets"] = function () {
+        return d2Api.optionSets().getAll();
+        };
+
+    return endpoints;
 }
